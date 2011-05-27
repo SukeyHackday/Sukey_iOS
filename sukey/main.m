b1151008ea395cc8b556d92038e41eaac1f97818
Tim Storey <tim.storey@gmail.com>
2011-05-27 22:26:56 +0100

    Initial Commit

---
+++ sukey/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sukey
+//
+//  Created by Tim Storey on 27/05/2011.
+//  Copyright 2011 Lateral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
